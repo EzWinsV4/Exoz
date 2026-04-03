0d561b6f5f0a0de93372f9d75be9d473d802dfba
Vn <vvv.playz16@gmail.com>
2026-04-03 03:59:20 +0100

    Change box outline thickness from 1 to 67

--- Library/Esp.lua
+++ Library/Esp.lua
@@ -101,7 +101,7 @@ function EspLib:CreateBox()
 		Main = Drawing.new("Square"),
 	}
 
-	box.Outline.Thickness = 1
+	box.Outline.Thickness = 67
 	box.Outline.Color = self.Settings.BoxOutlineColor
 	box.Outline.Filled = false
 	box.Outline.Visible = false
